f3999888e63fb2abc258210cc33fdfd11ccf7d8d
bee9827 <ehfrhfo9494@naver.com>
2024-11-10 03:09:05 +0900

    feat: Create stock list
    재고 리스트

---
+++ src/main/java/store/model/StockList.java
@@ -0,0 +1,45 @@
+package store.model;
+
+import java.text.DecimalFormat;
+import java.util.ArrayList;
+import java.util.List;
+
+public class StockList {
+    DecimalFormat decimalFormat = new DecimalFormat("###,###");
+    Inventory inventory;
+
+    public StockList(Inventory inventory) {
+        this.inventory = inventory;
+    }
+
+    public List<String> getStocks() {
+        List<String> stocks = new ArrayList<String>();
+        for (ProductPromotion item : inventory.getItems()) {
+            stocks.add(makeInfo(item));
+        }
+        return stocks;
+    }
+
+    private String makeInfo(ProductPromotion item) {
+        return getName(item.getProductName()) + getPrice(item.getProductPrice())
+                + getQuantity(item.getProductQuantity()) + getPromotionName(item.getPromotionName());
+    }
+
+    private String getName(String name) {
+        return name + " ";
+    }
+
+    private String getPrice(Long price) {
+        return STR."\{decimalFormat.format(price)}원 ";
+    }
+
+    private String getQuantity(Integer quantity) {
+        if (quantity == 0) return "재고 없음 ";
+        return STR."\{decimalFormat.format(quantity)}개 ";
+    }
+
+    private String getPromotionName(String promotionName) {
+        if (promotionName.equals("null")) return "";
+        return promotionName;
+    }
+}
